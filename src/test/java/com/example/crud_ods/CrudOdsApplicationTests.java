f225e0dcb5bc0491b5292ce4775ce18b677d505d
EGMJ <gabrieledson730@gmail.com>
2024-11-02 22:30:23 -0300

    feat: initial commit, add Spring Boot

---
+++ src/test/java/com/example/crud_ods/CrudOdsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crud_ods;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudOdsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
